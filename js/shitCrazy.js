9f0b715741ca8d275fdb2e5ee22310ce35b93b03
Iago de Sousa Oliveira <iago.oliveira@abcz.org.br>
2025-03-20 08:17:01 -0300

    💥fix: remoção fade de transição

--- js/shitCrazy.js
+++ js/shitCrazy.js
@@ -30,12 +30,8 @@ export function getConfigsShitCrazy(configs) {
 }
 
 export function setUpShitCrazy(setUpConfigs) {
-    // fade(setUpConfigs.img)
-    console.log(setUpConfigs.img)
     setUpConfigs.img.src = "/media/VacaAnimadav7.gif";
-    console.log(setUpConfigs.img)
-    unfade(setUpConfigs.img)
-    
+        
     setUpConfigs.eyeL.style.setProperty("background-color", "white");
     setUpConfigs.eyeR.style.setProperty("background-color", "white");
     setUpConfigs.eylidL.style.setProperty("top", "-7px");
@@ -47,30 +43,4 @@ export function setUpShitCrazy(setUpConfigs) {
     setUpConfigs.pupE.style.setProperty("width", "26px");
     setUpConfigs.pupR.style.setProperty("width", "26px");
     setUpConfigs.glasses.style.display = 'none';
-}
-
-function fade(element) {
-    var op = 1;  // initial opacity
-    var timer = setInterval(function () {
-        if (op <= 0.1){
-            clearInterval(timer);
-            element.style.display = 'none';
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op -= op * 0.1;
-    }, 10);
-}
-
-function unfade(element) {
-    var op = 0.1;  // initial opacity
-    element.style.display = 'block';
-    var timer = setInterval(function () {
-        if (op >= 1){
-            clearInterval(timer);
-        }
-        element.style.opacity = op;
-        element.style.filter = 'alpha(opacity=' + op * 100 + ")";
-        op += op * 0.1;
-    }, 10);
 }
