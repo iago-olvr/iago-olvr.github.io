382b950abd4f2b9b24cd32adc9cda5d272db087f
Iago de Sousa Oliveira <iago.oliveira@abcz.org.br>
2025-03-28 14:09:16 -0300

    🚧 feat: Criação do Helper

--- js/moood.js
+++ js/moood.js
@@ -25,14 +25,18 @@ import { setUpDVD } from '/js/dvd.js';
 
 // --------------- VAR ---------------
 
+let body = document.body;
+
 let hover = document.getElementsByClassName("hover")[0];
 
 let front = document.getElementsByClassName("front")[0];
 let sky = document.getElementsByClassName("sky")[0];
 let img = document.getElementById("img");
 let imgBarn = document.getElementById("barn");
-
+let imgElderlys = document.getElementById("elderlys");
 let currentHumor = document.getElementById("currentHumor");
+let help = document.getElementById("help");
+
 let humorRadio = document.querySelectorAll("input[type='radio']");
 let labels = document.querySelectorAll("label");
 
@@ -43,6 +47,8 @@ let pupR = document.getElementsByClassName("pupR")[0];
 let eylidL = document.getElementsByClassName("eylidL")[0];
 let eylidR = document.getElementsByClassName("eylidR")[0];
 let glasses = document.getElementsByClassName("glasses")[0];
+let elderlys = document.getElementsByClassName("elderlys")[0];
+
 
 let dialog = document.getElementsByClassName("dialog")[0];
 
@@ -140,6 +146,15 @@ hover.onmousemove = (e) => {
 hover.onmouseleave = (e) => {
     mouseleave();
 };
+
+help.onclick = (e) => {
+    getHelp();
+};
+
+imgElderlys.onclick = (e) => {
+    disposeHelp();
+}
+
 
 // --------------- FUNCTIONS ---------------
 
@@ -339,8 +354,8 @@ function mouseleave() {
 
     if (configs.time == "N") { //Night
         imgBarn.src = "/media/Barnv3.png";
-        document.body.style.backgroundImage = "url('/media/backgroundv2.png')";
-        document.body.style.backgroundColor = "#074505";
+        body.style.backgroundImage = "url('/media/backgroundv2.png')";
+        body.style.backgroundColor = "#074505";
         sky.style.setProperty("display", "none");
         labels.forEach((e) => {
             e.style.setProperty("color", "darkslategray");
@@ -351,8 +366,8 @@ function mouseleave() {
         })
     } else { //Day
         imgBarn.src = "/media/Barnv2.png";
-        document.body.style.backgroundImage = "url('/media/background.png')";
-        document.body.style.backgroundColor = "#11ac0d";
+        body.style.backgroundImage = "url('/media/background.png')";
+        body.style.backgroundColor = "#11ac0d";
         sky.style.setProperty("display", "block");
         labels.forEach((e) => {
             e.style.setProperty("color", "papayawhip");
@@ -386,6 +401,26 @@ function mouseleave() {
         default:
             break;
     }
+}
+
+function getHelp() {
+    elderlys.style.display = "inline-block";
+    document.querySelectorAll('.focus').forEach(el => {
+        el.style.filter = "blur(4px)";
+    })
+}
+
+function disposeHelp() {
+    elderlys.style.setProperty("animation", "byebyeElderlys 500ms");
+    document.querySelectorAll('.focus').forEach(el => {
+        el.style.filter = "none";
+    })
+    setTimeout(resetsHelp, 450);
+}
+
+function resetsHelp() {
+    elderlys.style.display = "none";
+    elderlys.style.setProperty("animation", "helloElderlys 500ms");
 }
 
 initialConfig();
